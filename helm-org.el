8082ffca2aa5744cc3e1f6177ecb576f0a4be721
Matus Goljer <dota.keys@gmail.com>
2014-12-14 22:25:42 +0100

    Add org search

--- helm-org.el
+++ helm-org.el
@@ -104,6 +104,111 @@ NOTE: This will be slow on large org buffers."
         :candidate-number-limit 99999
         :buffer "*helm org inbuffer*"))
 
+;;; Org search
+;;
+;;
+
+(defun helm-org-goto-char (point)
+  (switch-to-buffer helm-current-buffer)
+  (goto-char point)
+  (org-show-context)
+  (org-show-entry))
+
+(defun helm-org-search-get-candidates-in-file ()
+  ;; TODO: add support for multiple buffers
+  (with-current-buffer helm-current-buffer
+    (save-excursion
+      (goto-char (point-min))
+      (outline-next-heading)
+      (let ((words (split-string helm-pattern))
+            (match t)
+            todo tags search headline re)
+        (mapc
+         (lambda (word)
+           (cond
+            ((and (> (length word) 1) (= ?! (aref word 0))) (push (substring word 1) todo))
+            ((and (> (length word) 1) (= ?: (aref word 0))) (push (substring word 1) tags))
+            ((and (> (length word) 1) (= ?* (aref word 0))) (push (substring word 1) headline))
+            (t (push word search))))
+         words)
+        (setq search (nreverse search))
+        (while match
+          (let ((header (when (or todo headline) (org-heading-components)))
+                tags-at)
+            (when (and
+                   ;; sideffect search!
+                   (or (not search) (setq match (re-search-forward (car search) nil t)))
+                   (or (not (> (length search) 1))
+                       (let ((limit (save-excursion (or (outline-next-heading) (point-max)))))
+                         (every (lambda (p)
+                                  (org-back-to-heading t)
+                                  (re-search-forward p limit t))
+                                (cdr search))))
+                   (or (not todo) (every (lambda (m) (string-match-p m (or (nth 2 header) ""))) todo))
+                   (or (not tags) (progn (setq tags-at (org-get-tags-at)) (every (lambda (m) (org-match-any-p m tags-at)) tags)))
+                   (or (not headline) (every (lambda (m) (string-match-p m (nth 4 header))) headline)))
+              (push (cons
+                     (save-excursion
+                       (org-back-to-heading t)
+                       (let* ((lb (line-beginning-position))
+                              (le (line-end-position))
+                              (heading (progn
+                                         (font-lock-fontify-region lb le)
+                                         (looking-at org-todo-line-tags-regexp)
+                                         (buffer-substring lb (or (match-beginning 4) le)))))
+                         (concat
+                          (replace-regexp-in-string
+                           "[[:space:]]+" " "
+                           (concat
+                            heading " "
+                            ;; add all tags, including inherited
+                            (if (or tags-at (setq tags-at (org-get-tags-at)))
+                                (propertize (concat ":" (mapconcat 'identity tags-at ":") ":")
+                                            'face (get-text-property 0 'face heading))
+                              "")))
+                          ;; only display context if the match isn't in
+                          ;; the headline itself and we made a search
+                          (if (or (not match) (and (< lb match) (< match le))) ""
+                            (progn
+                              (goto-char match)
+                              (concat "    " (substring (thing-at-point 'line) 0 -1)))))))
+                     (point))
+                    re)))
+          (setq match (and match (outline-next-heading))))
+        (nreverse re)))))
+
+(defvar helm-org-source-search
+  '((name . "Org search")
+    ;; (init . (lambda ()
+    ;;           (setq helm-org-search-pattern nil)
+    ;;           (helm-candidate-buffer helm-current-buffer)))
+    ;; (candidates-in-buffer)
+    (candidates . helm-org-search-get-candidates-in-file)
+    (volatile . t)
+    (match (lambda (_) t))
+    ;; (search helm-org-search-forward)
+    ;; (get-line . helm-org-search-get-line)
+    ;; cache the parsed pattern, so we don't have to recompute it each
+    ;; time we search forward for a new match
+    ;; (pattern-transformer helm-org-search-parse-pattern-and-cache)
+    ;; TODO: add persistent-action
+    ;; (persistent-action . helm-regexp-persistent-action)
+    (persistent-help . "Show this line")
+    (nomark)
+    (no-matchplugin)
+    (requires-pattern . 2)
+    (mode-line . "Press TAB to select action.")
+    (action ("Go to line" . helm-org-goto-char)
+            ("Refile to this heading" . helm-org-heading-refile)
+            ("Insert link to this heading"
+             . helm-org-insert-link-to-heading-at-marker))))
+
+;;;###autoload
+(defun helm-org-in-buffer-search ()
+  (interactive)
+  (helm :sources 'helm-org-source-search
+        :candidate-number-limit 99999
+        :buffer "*helm org search*"))
 
 (provide 'helm-org)
 
